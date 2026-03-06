13f263c088c00b669fc2cf9ca3ab55ed88b42ae9
Stephan Maihöfer <stephan@undev.de>
2026-03-06 17:52:08 +0100

    logging

--- test.js
+++ test.js
@@ -4,9 +4,13 @@ const cables = new CablesCLI();
 cables.export({
     "patch": ["99CJma"],
     "destination": "test",
-    "url": "http://dev.cables.local"
-}).then(() => {
-    console.log("Export finished!");
-}).catch((e) => {
-    console.log("There was an error exporting your patch :/", e.toString());
-});
+    "url": "http://dev.cables.local",
+})
+    .then((result) =>
+    {
+        console.log("Export finished!", result.log.map((l) => { return l.message; }));
+    })
+    .catch((e) =>
+    {
+        console.log("There was an error exporting your patch :/", e.toString());
+    });
